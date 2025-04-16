4f5c78454b202958deef21d0597d7b60221be0aa
Jay <jay.jrjren@gmail.com>
2025-04-15 22:31:08 -0400

    chore: update manifest.ts formatting and add localhost permission

--- chrome-extension/manifest.ts
+++ chrome-extension/manifest.ts
@@ -1,6 +1,6 @@
-import { readFileSync } from 'node:fs';
+import { readFileSync } from 'node:fs'
 
-const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
+const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'))
 
 /**
  * @prop default_locale
@@ -18,51 +18,51 @@ const packageJson = JSON.parse(readFileSync('./package.json', 'utf8'));
  * css: ['content.css'], // public folder
  */
 const manifest = {
-  manifest_version: 3,
-  default_locale: 'en',
-  name: '__MSG_extensionName__',
-  browser_specific_settings: {
-    gecko: {
-      id: 'example@example.com',
-      strict_min_version: '109.0',
-    },
-  },
-  version: packageJson.version,
-  description: '__MSG_extensionDescription__',
-  host_permissions: ['<all_urls>'],
-  permissions: ['storage', 'scripting', 'tabs', 'notifications'],
-  background: {
-    service_worker: 'background.js',
-    type: 'module',
-  },
-  action: {
-    default_popup: 'popup/index.html',
-    default_icon: 'icon-34.png',
-  },
-  chrome_url_overrides: {},
-  icons: {
-    '128': 'icon-128.png',
-  },
-  content_scripts: [
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content/index.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      js: ['content-ui/index.iife.js'],
-    },
-    {
-      matches: ['http://*/*', 'https://*/*', '<all_urls>'],
-      css: ['content.css'],
-    },
-  ],
-  web_accessible_resources: [
-    {
-      resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
-      matches: ['*://*/*'],
-    },
-  ],
-} satisfies chrome.runtime.ManifestV3;
+	manifest_version: 3,
+	default_locale: 'en',
+	name: '__MSG_extensionName__',
+	browser_specific_settings: {
+		gecko: {
+			id: 'example@example.com',
+			strict_min_version: '109.0',
+		},
+	},
+	version: packageJson.version,
+	description: '__MSG_extensionDescription__',
+	host_permissions: ['<all_urls>', 'http://localhost:11434/*'],
+	permissions: ['storage', 'scripting', 'tabs', 'notifications'],
+	background: {
+		service_worker: 'background.js',
+		type: 'module',
+	},
+	action: {
+		default_popup: 'popup/index.html',
+		default_icon: 'icon-34.png',
+	},
+	chrome_url_overrides: {},
+	icons: {
+		'128': 'icon-128.png',
+	},
+	content_scripts: [
+		{
+			matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+			js: ['content/index.iife.js'],
+		},
+		{
+			matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+			js: ['content-ui/index.iife.js'],
+		},
+		{
+			matches: ['http://*/*', 'https://*/*', '<all_urls>'],
+			css: ['content.css'],
+		},
+	],
+	web_accessible_resources: [
+		{
+			resources: ['*.js', '*.css', '*.svg', 'icon-128.png', 'icon-34.png'],
+			matches: ['*://*/*'],
+		},
+	],
+} satisfies chrome.runtime.ManifestV3
 
-export default manifest;
+export default manifest

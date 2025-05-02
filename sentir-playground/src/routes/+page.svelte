a7f9493d75ae21bf8c517380224eef0a7150ecaf
Jay <jay.jrjren@gmail.com>
2025-05-01 22:11:33 -0400

    chore: add tall div to playground so we can test scrolling

--- sentir-playground/src/routes/+page.svelte
+++ sentir-playground/src/routes/+page.svelte
@@ -12,7 +12,7 @@
 </head>
 <div class="flex flex-col gap-4 w-96">
 	<div class="flex ml-1 mt-2 mb-3 mr-4 p-2 border border-gray-300">
-		<input id="input-1" type="text" bind:value={text} class="ml-1 mt-2 mb-3 mr-4 px-2 py-1 rounded-md border border-gray-300" placeholder="Product Search" />
+		<input id="input-1" type="text" bind:value={text} class="w-full ml-1 mt-2 mb-3 mr-4 pl-2 pt-1 pb-3 rounded-md border border-gray-300" placeholder="Product Search" />
 	</div>
 	<textarea id="textarea-1" bind:value={text2} placeholder="Why are you applying for this job?"></textarea>
 	<button onclick={() => showInput2 = !showInput2} class="border p-2 rounded-md">Toggle Input 2</button>
@@ -24,4 +24,7 @@
 	{#if showInput3}
 		<input id="input-3" bind:value={text4} placeholder="What's the meaning of life?" />
 	{/if}
+	<div class="flex flex-col justify-between h-[1200px] border border-gray-300 rounded-md">
+		Very tall div so that we can test scroll
+	</div>
 </div>

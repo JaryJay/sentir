aa6e858190fce3f595242c13818c2d94023f1f05
Jay <jay.jrjren@gmail.com>
2025-05-07 11:00:19 -0400

    feat: implement caching for AI completions and structured response schema

--- sentir-api/src/api/v0/complete.ts
+++ sentir-api/src/api/v0/complete.ts
@@ -1,9 +1,29 @@
-import { GoogleGenAI } from "@google/genai";
+import { GoogleGenAI, Type } from "@google/genai";
 import { PROCESS_ENV } from "@/utils/env";
 import { options } from "@/utils/options";
 import { CompletionsResponse, PromptRequest } from "sentir-common";
 
+const SYSTEM_INSTRUCTION = `
+You are an intelligent form completion assistant, similar to GitHub Copilot or Cursor IDE.
+Given the following context of a web page and the current content of an input/textarea field, predict the most likely word or phrase the user will type to complete the field.
+You are allowed to modify the current field value, for example to fix typos.
+Do not enclose the completion in quotes. Output the ENTIRE completion as a string.
+For example, if the current field value is "howw to ceter a" and the site is Stack Overflow, the completion should be "how to center a div".
+As another example, if the current field value is "Asus" and the site is Amazon, the completion should be "Asus laptop", not just "laptop".
+Similarly, if the current field value is "how" and the site is Google, the completion should be "how to", not just "to".
+A final example, if the current field value is "hel", the completion should be "hello". Do not just output "lo".
+You do not have to complete a lot of words. Even just the next 1 or 2 words are enough.
+Do not output a newline character at the end, unless you are sure it is part of the completion.
+`;
+
 const ai = new GoogleGenAI({ apiKey: PROCESS_ENV.GEMINI_API_KEY });
+const cache = await ai.caches.create({
+  model: "gemini-2.0-flash-lite",
+  config: {
+    systemInstruction: SYSTEM_INSTRUCTION,
+    ttl: "43200s",
+  },
+});
 
 async function complete(req: Request): Promise<Response> {
   const parsed = PromptRequest.safeParse(await req.json());
@@ -18,13 +38,36 @@ async function complete(req: Request): Promise<Response> {
 
   const timestamp = Date.now();
 
-  // Call ollama local server
+  // TODO: Use cached system instruction
   const response = await ai.models.generateContent({
     model: "gemini-2.0-flash-lite",
     contents: prompt,
     config: {
       candidateCount: 2,
-      systemInstruction: SYSTEM_INSTRUCTION,
+      cachedContent: cache.name,
+      responseSchema: {
+        type: Type.OBJECT,
+        properties: {
+          completionType: {
+            type: Type.STRING,
+            description:
+              "The type of completion to perform. If the completion only adds to the current text, use 'insert'. If the completion replaces a part of the current text, use 'replace'.",
+            enum: ["insert", "replace"],
+          },
+          textToReplace: {
+            type: Type.STRING,
+            description:
+              "The text to replace. Only present if completionType is 'replace'.",
+            example: "w",
+            nullable: true,
+          },
+          completion: {
+            type: Type.STRING,
+            description: "The completion to perform",
+            example: "a few words",
+          },
+        },
+      },
     },
   });
 
@@ -36,28 +79,21 @@ async function complete(req: Request): Promise<Response> {
 
   console.dir(response, { depth: null });
 
-  const completions = response.candidates
+  const rawCompletions = response.candidates
     ?.map((c) => c.content?.parts?.map((p) => p.text)?.join(""))
     .filter((s) => s !== undefined) ?? [response.text];
+
+  // The LLM will often add an unecessary \n on the end
+  // This is a hack to remove that (but only 1 instance because sometimes you actually want to enter newlines)
+  const completions = rawCompletions.map((text) =>
+    text.endsWith("\n") ? text.slice(0, text.length - 1) : text
+  );
 
   return new Response(
     JSON.stringify({ completions, timestamp } satisfies CompletionsResponse),
     { status: 200 }
   );
 }
-
-const SYSTEM_INSTRUCTION = `
-You are an intelligent form completion assistant, similar to GitHub Copilot or Cursor IDE.
-Given the following context of a web page and the current content of an input/textarea field, predict the most likely word or phrase the user will type to complete the field.
-You are allowed to modify the current field value, for example to fix typos.
-Do not enclose the completion in quotes. Output the ENTIRE completion as a string.
-For example, if the current field value is "howw to ceter a" and the site is Stack Overflow, the completion should be "how to center a div".
-As another example, if the current field value is "Asus" and the site is Amazon, the completion should be "Asus laptop", not just "laptop".
-Similarly, if the current field value is "how" and the site is Google, the completion should be "how to", not just "to".
-A final example, if the current field value is "hel", the completion should be "hello". Do not just output "lo".
-You do not have to complete a lot of words. Even just the next 1 or 2 words are enough.
-Do not output a newline character at the end, unless you are sure it is part of the completion.
-`;
 
 function constructPrompt(request: PromptRequest) {
   return `

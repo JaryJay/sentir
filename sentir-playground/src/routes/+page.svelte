1e90e6a38e1ac21e1d5e6b49532555a269fad5ff
Jay <jay.jrjren@gmail.com>
2025-05-09 14:40:42 -0400

    fix: use svelte:head instead of head in sentir playground

--- sentir-playground/src/routes/+page.svelte
+++ sentir-playground/src/routes/+page.svelte
@@ -7,9 +7,9 @@
 	let showInput3 = $state(false);
 </script>
 
-<head>
+<svelte:head>
 	<title>Sentir Playground</title>
-</head>
+</svelte:head>
 <div class="flex flex-col gap-4 w-96">
 	<div class="flex ml-1 mt-2 mb-3 mr-4 p-2 border border-gray-300">
 		<input id="input-1" type="text" bind:value={text} class="w-full ml-1 mt-2 mb-3 mr-4 pl-2 pt-1 pb-3 rounded-md border border-gray-300" placeholder="Product Search" />
